8cd5b55cf4e8418f1499aa2ee18009f140d261ce
Jorge-GustavoAlencarOliveira <jorgegustavo500@gmail.com>
2023-08-29 15:22:17 -0300

    ultima vez

--- src/middlewares/isAuthenticated.ts
+++ src/middlewares/isAuthenticated.ts
@@ -1,4 +1,3 @@
-import 'dotenv/config';
 import { Request, Response, NextFunction, response } from 'express';
 import { verify } from 'jsonwebtoken';
 
@@ -18,7 +17,7 @@ export function isAuthenticated(
   const [, token] = authToken.split(' ');
 
   try {
-    const { sub } = verify(token, process.env.JWT_SECRET) as Payload;
+    const { sub } = verify(token, `${process.env.JWT_SECRET}`) as Payload;
 
     req.user_id = sub
     

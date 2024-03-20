26e491153575d66ba93a49152d98a2e6509f0d58
Jorge-GustavoAlencarOliveira <jorgegustavo500@gmail.com>
2024-03-20 18:34:38 -0300

    novo

--- src/controllers/project/CreateProjectController.ts
+++ src/controllers/project/CreateProjectController.ts
@@ -3,12 +3,10 @@ import { CreateProjectService } from '../../services/projects/CreateProjectServi
 
 class CreateProjectController {
   async handle(req: Request, res: Response) {
-    // const { dados } = req.body;
     const user_id = req.user_id;
     const createProjectService = new CreateProjectService();
     const project = await createProjectService.execute({
       user_id,
-      // dados,
     });
     return res.json(project);
   }
--- src/services/projects/CreateProjectService.ts
+++ src/services/projects/CreateProjectService.ts
@@ -2,42 +2,17 @@ import prismaClient from '../../prisma';
 
 interface projectRequest {
   user_id: string;
-  // dados: Prisma.JsonObject;
 }
 
 class CreateProjectService {
   async execute({user_id}: projectRequest) {
-    const numberProjects = await prismaClient.project.count({
-      where: {
-        user_id: user_id,
-      },
-    });
-
-    const userSubscriptions = await prismaClient.user.findFirst({
-      where: {
-        id: user_id,
-      },
-      select: {
-        subscriptions: true,
-      },
-    });
-
-    // if (
-    //   userSubscriptions?.subscriptions?.status !== 'active' &&
-    //   numberProjects >= 3
-    // ) {
-    //   throw new Error('não autorizado');
-    // }
-
     const project = await prismaClient.project.create({
       data: {
         user_id: user_id,
-        // dados: dados
       },
       select: {
         id: true,
         status: true,
-        // dados: true,
       },
     });
     return project;
@@ -45,3 +20,25 @@ class CreateProjectService {
 }
 
 export { CreateProjectService };
+
+ // const numberProjects = await prismaClient.project.count({
+    //   where: {
+    //     user_id: user_id,
+    //   },
+    // });
+
+    // const userSubscriptions = await prismaClient.user.findFirst({
+    //   where: {
+    //     id: user_id,
+    //   },
+    //   select: {
+    //     subscriptions: true,
+    //   },
+    // });
+
+    // if (
+    //   userSubscriptions?.subscriptions?.status !== 'active' &&
+    //   numberProjects >= 3
+    // ) {
+    //   throw new Error('não autorizado');
+    // }
